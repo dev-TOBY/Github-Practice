e66d9e3154924b6ce18404e9b8dc31b83f7d66c7
Yester <yechan123456@gmail.com>
2022-11-10 22:48:35 +0900

    Initial Commit

---
+++ Github Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github Practice
+//
+//  Created by 예찬 on 2022/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
